86c84934e7b36622463859e1d1ba701160e06bd3
Alejosd <alejosd5@gmail.com>
2016-09-17 13:26:27 -0300

    declaracion de constantes básicas en GoLang

--- main/sdWorld.go
+++ main/sdWorld.go
@@ -1,14 +1,15 @@
 package main
 
 import "fmt"
+import "math"
 
 func main() {
 
 	var helloWorld string = "helloworld"
 	fmt.Println("Rock And Roll "+helloWorld)
 
-	var a,b int = 5,10
-	fmt.Println("5+10 =",a+b)
+	var a,b float64 = 8.5,25.0
+	fmt.Println(a/b)
 
 	var t = true
 	var f = false
@@ -21,6 +22,22 @@ func main() {
 	var entero int
 	fmt.Println(entero)
 
+	//declaracion corta
 
+	z:= "short"
+	fmt.Println(z)
+
+	//constantes
+
+	const n = 500
+	const d = 3e20 / n
+
+	fmt.Println(n)
+
+	fmt.Println(d)
+
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
 
 }
