1b150c05952f96b20b68d30496f5d4238c954402
Alejosd <alejosd5@gmail.com>
2016-09-17 16:41:12 -0300

    Ejemplo Struct con GoLang

--- main/arrays.go
+++ main/arrays.go
@@ -1,28 +1,62 @@
+// Structs
+
+// Los ejercicios:
+// Declarar un struct para mantener info. de un usuario (nombre, dirección, edad)
+
+// Crear un valor y lo vamos a inicializar con valores
+// Mostrar cada campo
+// Declarar e inicializar un struct anónimo con los mismos 3 campos
+// Mostrar el valor.
+
 package main
 
 import "fmt"
 
-func main(){
+// usuario representa un usuario en el sistema
 
-	numeros := []int{2,4,6}
-	suma := 0
-	for _, num:= range numeros {
-		suma += num
-	}
-
-	fmt.Println("suma:", suma)
-
-	for i, numero :=range numeros{
-		if numero == 3 {
-			fmt.Println("index:", i)
-		}
-	}
-
-
-	mapa := map[string]string{"a":"auto","b":"bebe"}
-	for key, value:=range mapa{
-		fmt.Println(key,value)
-	}
+type usuario struct {
+	nombre string
+	direccion string
+	edad int
 }
+
+// main es el punto de entrada de nuestras aplicaciones/ sistemas
+
+func main() {
+	// Declarar la variable usuario y la iniciamos usando un struct
+
+	vero := usuario{
+		nombre : "Verónica",
+		direccion : "Calle 12",
+		edad : 38,
+	}
+
+	//Mostrar los valores de cada campo
+
+	fmt.Println("Nombre", vero.nombre)
+	fmt.Println("Dirección", vero.direccion)
+	fmt.Println("Age", vero.edad)
+
+	// Declarar otro struct anon con los mismos 3 campos
+	nicole := struct {
+		nombre string
+		direccion string
+		edad int
+	} {
+
+		nombre: "Nicole",
+		direccion : "Calle 13",
+		edad: 22,
+	}
+
+	// Imprimir datos de Nicole
+
+	fmt.Println("Nombre", nicole.nombre)
+	fmt.Println("Dirección", nicole.direccion)
+	fmt.Println("Edad", nicole.edad)
+}
+
+
+
 
 
