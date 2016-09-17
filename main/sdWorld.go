fe6f26e4fe393ca0bfdcb4dd8f9f741ff1f19c7c
Alejosd <alejosd5@gmail.com>
2016-09-17 13:00:56 -0300

    declaracion de variables básicas en GoLang

--- main/sdWorld.go
+++ main/sdWorld.go
@@ -3,15 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	
-	fmt.Println("Rock And Roll "+ "helloworld")
-	fmt.Println("5+10 =",5+10)
-	fmt.Println("7/3 =",7/3)
-	fmt.Println("7.0/3.0 =",7.0/3.0)
 
-	fmt.Println(true&&false)
-	fmt.Println(true||false)
-	fmt.Println(!false)
+	var helloWorld string = "helloworld"
+	fmt.Println("Rock And Roll "+helloWorld)
+
+	var a,b int = 5,10
+	fmt.Println("5+10 =",a+b)
+
+	var t = true
+	var f = false
+
+	fmt.Println(t&&f)
+	fmt.Println(t ||f)
+	fmt.Println(!f)
+
+	//declaracion variable
+	var entero int
+	fmt.Println(entero)
 
 
 
