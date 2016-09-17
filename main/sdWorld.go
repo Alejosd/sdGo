5bda42555a14784f7519703eeb88da2682e89c6b
Alejosd <alejosd5@gmail.com>
2016-09-17 12:55:31 -0300

    operaciones  básicas en GoLang

--- main/sdWorld.go
+++ main/sdWorld.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Rock And Roll")
+	
+	fmt.Println("Rock And Roll "+ "helloworld")
+	fmt.Println("5+10 =",5+10)
+	fmt.Println("7/3 =",7/3)
+	fmt.Println("7.0/3.0 =",7.0/3.0)
+
+	fmt.Println(true&&false)
+	fmt.Println(true||false)
+	fmt.Println(!false)
+
+
+
 }
